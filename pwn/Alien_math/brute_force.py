7b72f97ecb0e6d0d253ab668d33d229a6bf92e75
M-Pepper <46696465+M-Pepper@users.noreply.github.com>
2021-09-13 20:02:13 -0400

    Create brute_force.py

---
+++ pwn/Alien_math/brute_force.py
@@ -0,0 +1,22 @@
+!#/usr/bin/env python3
+#
+# Not my script
+# Borrowed from https://github.com/apoirrier/CTFs-writeups/blob/master/CSAWQual2021/pwn/AlienMath.md
+#
+
+goal = "7759406485255323229225"
+
+def modify(x, y, i):
+    b = ctypes.c_uint((x*0x30 + (x+i)*0xb - 4)).value % 10
+    b += y
+    return b % 10
+
+print(7, end="")
+current = 7
+for i in range(21):
+    g = int(goal[i+1])
+    for next in range(10):
+        if modify(current, next, i) == g:
+            print(next, end="")
+            current = g
+            break
